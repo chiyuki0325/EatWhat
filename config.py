7f3c352debb51ff12f11ac97a611d63bc69f19ad
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-06-30 21:09:58 +0800

    🔧 修复公开群

--- config.py
+++ config.py
@@ -7,14 +7,15 @@ from dotenv import load_dotenv
 class Config:
     api_id: str
     api_hash: str
-    chat_id: int
+    chat_id: int | str
     easter_eggs: list[str]
 
 
 load_dotenv()
+chat_id = getenv("CHAT_ID")
 config = Config(
     api_id=getenv("API_ID"),
     api_hash=getenv("API_HASH"),
-    chat_id=int(getenv("CHAT_ID")),
+    chat_id=int(chat_id) if chat_id.isdigit() else chat_id,
     easter_eggs=getenv("EASTER_EGGS").split(","),
 )
